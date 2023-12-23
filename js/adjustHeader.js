f85973fedd8dcfa3f5589ade2d26d7aecd3b3569
BrandonWeir <bw@rmts2.com>
2023-12-22 20:44:55 -0700

    revert 3

--- js/adjustHeader.js
+++ js/adjustHeader.js
@@ -12,20 +12,3 @@ function adjustPseudoElement() {
 
 // Call the function once to set the initial height
 adjustPseudoElement();
-
-// iiiiiiii
-
-// function adjustPseudoElement() {
-//     const header = document.querySelector('header');
-//     const headerHeight = header.offsetHeight;
-//     const sections = document.querySelectorAll('section');
-
-//     // Set the --header-height variable on the :root element
-//     document.documentElement.style.setProperty('--header-height', `${headerHeight}px`);
-// }
-
-// // Call the function once to set the initial height
-// adjustPseudoElement();
-
-// // Update the --header-height variable whenever the window is resized
-// window.addEventListener('resize', adjustPseudoElement);
